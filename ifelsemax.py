116c67d324bc2a6f1ddc1dbc6c55b6b8cccec3d5
mkkunal <132072421+mkkunal@users.noreply.github.com>
2023-04-28 19:04:29 +0530

    Add files via upload

---
+++ ifelsemax.py
@@ -0,0 +1,10 @@
+a=int(input("enter no.a--->"))
+b=int(input("enter no.b--->"))
+c=int(input("enter no.c--->"))
+if a>b and a>c:
+    print("a is largest")
+elif b>a and b>c:
+    print("b is largest")
+else:
+    print("c is largest")
+
